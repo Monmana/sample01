6ee4e67a6ed7d11003b53eaa66cea2ee5f060338
Monmana <156147567+Monmana@users.noreply.github.com>
2024-01-10 10:08:46 +0900

    Create humberger.js

---
+++ humberger.js
@@ -0,0 +1,11 @@
+(()=>{
+    'use strict';
+
+$(function(){
+    $('.menu-trigger').on('click', function(){
+        $(this).toggleClass('active');
+        $('.global-nav').toggleClass('active');
+    });
+});
+
+})();
